9ffef7a5a6adc2ce5ae75a692aadf49be51ddf03
Bruno Lange <blangeram@gmail.com>
2019-01-27 16:41:49 -0800

    Simplifies caching

--- artifax/models.py
+++ artifax/models.py
@@ -3,39 +3,34 @@ from . import utils
 
 class Artifax:
     def __init__(self, dic={}):
-        self._data = dic
+        self._artifacts = dic
         self._graph = None
-        self._cache = {}
+        self._result = None
+
+    def set(self, node, value):
+        if node in self._artifacts:
+            self._revoke(node)
+        self._artifacts[node] = value
 
     def _revoke(self, node):
-        _ = self._cache.pop(node)
+        _ = self._result.pop(node)
         for k in self.graph()[node]:
             self._revoke(k)
 
-    def set(self, node, value):
-        if node in self._data:
-            self._revoke(node)
-        self._data[node] = value
-
     def pop(self, node):
-        return self._data.pop(node)
+        return self._artifacts.pop(node)
 
     def graph(self):
-        return utils.to_graph(self._data)
+        return utils.to_graph(self._artifacts)
 
     def sort(self):
         return utils.topological_sort(self.graph())
 
-    def _build(self):
-        nodes = self.sort()
-        return builder.assemble(self._data, nodes, cache=self._cache)
-
     def build(self):
-        result = self._build()
-        for k, v in result.items():
-            if not callable(v):
-                self._cache[k] = v
-        return result
+        nodes = self.sort()
+        result = builder.assemble(self._artifacts, nodes, cache=self._result or {})
+        self._result = result
+        return result.copy()
 
     def __len__(self):
-        return len(self._data)
+        return len(self._artifacts)
