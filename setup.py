f41d2374d4a735ae9221f9a65d123c39852d0d2e
Bruno Lange <blangeram@gmail.com>
2019-09-20 23:30:27 -0700

    Registers version 0.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as handle:
 
 setup(
     name='artifax',
-    version='0.1.2',
+    version='0.1.3',
     packages=['artifax'],
     description='python package for building artifacts from a computational graph',
     author='Bruno Lange',
