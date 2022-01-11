b132670f904a3c54e33f03d27c45ae6381b734c6
Bruno Lange <blangeram@gmail.com>
2022-01-10 19:22:20 -0800

    Sets requirement for Python at >=3.5

--- setup.py
+++ setup.py
@@ -1,29 +1,24 @@
-
 from setuptools import setup
 from os import path
 
-with open('requirements.txt') as handle:
+with open("requirements.txt") as handle:
     requirements = handle.readlines()
 
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name='artifax',
-    version='0.1.3.1',
-    packages=['artifax'],
-    description='python package for building artifacts from a computational graph',
+    name="artifax",
+    version="0.2",
+    packages=["artifax"],
+    description="python package for building artifacts from a computational graph",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    author='Bruno Lange',
-    author_email='blangeram@gmail.com',
-    url='https://gitlab.com/brunolange/artifax',
+    long_description_content_type="text/markdown",
+    author="Bruno Lange",
+    author_email="blangeram@gmail.com",
+    url="https://gitlab.com/brunolange/artifax",
     install_requires=requirements,
-    python_requires='>=3',
-    extras_require={
-        'dev': [
-            'pylint'
-        ]
-    }
+    python_requires=">=3.5",
+    extras_require={"dev": ["pylint"]},
 )
