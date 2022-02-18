b1636a06a2669aebaae5866312a2a3515bb32fb4
Bruno Lange <blangeram@gmail.com>
2022-02-17 18:17:04 -0800

    Formats tests with black

--- test/test_build.py
+++ test/test_build.py
@@ -5,84 +5,80 @@ import json
 from artifax import build, At
 from artifax.exceptions import UnresolvedDependencyError
 
-class BuildTest(unittest.TestCase):
 
+class BuildTest(unittest.TestCase):
     def test_empty_build(self):
         artifacts = {}
         result = build(artifacts)
         self.assertEqual(result, {})
 
     def test_single_artifact_build(self):
-        artifacts = {'a': 42}
+        artifacts = {"a": 42}
         result = build(artifacts)
         self.assertEqual(len(result), 1)
-        self.assertEqual(result, {'a': 42})
+        self.assertEqual(result, {"a": 42})
 
     def test_artifact_immutability(self):
-        artifacts = {
-            'a': lambda: 42,
-            'b': lambda a: a()**2
-        }
+        artifacts = {"a": lambda: 42, "b": lambda a: a() ** 2}
 
-        results = build(artifacts, solver='async')
+        results = build(artifacts, solver="async")
 
         self.assertTrue(isinstance(artifacts, dict))
         self.assertEqual(len(artifacts), 2)
-        self.assertEqual(set(artifacts.keys()), {'a', 'b'})
-        self.assertTrue(callable(artifacts['b']))
-        self.assertFalse(callable(results['b']))
+        self.assertEqual(set(artifacts.keys()), {"a", "b"})
+        self.assertTrue(callable(artifacts["b"]))
+        self.assertFalse(callable(results["b"]))
 
     def test_constant_artifacts_build(self):
         obj = object()
         artifacts = {
-            'int': 42,
-            'string': 'Hello',
-            'obj': obj,
-            'list': [1,2,3],
-            'dictionary': {'answer': 42},
-            'set': set([1,2,3.14])
+            "int": 42,
+            "string": "Hello",
+            "obj": obj,
+            "list": [1, 2, 3],
+            "dictionary": {"answer": 42},
+            "set": set([1, 2, 3.14]),
         }
-        result = build(artifacts, solver='linear') # async solver copies objects to subprocess!!
+        result = build(
+            artifacts, solver="linear"
+        )  # async solver copies objects to subprocess!!
         for k in artifacts:
             self.assertEqual(result[k], artifacts[k])
 
     def test_sample_build(self):
         artifacts = {
-            'A': 42,
-            'B': lambda: 7,
-            'C': lambda: 10,
-            'AB': lambda A, B: A + B(),
-            'C minus B': lambda B, C: C() - B(),
-            'greet': 'Hello',
-            'msg': lambda greet, A: '{} World! The answer is {}.'.format(greet, A),
+            "A": 42,
+            "B": lambda: 7,
+            "C": lambda: 10,
+            "AB": lambda A, B: A + B(),
+            "C minus B": lambda B, C: C() - B(),
+            "greet": "Hello",
+            "msg": lambda greet, A: "{} World! The answer is {}.".format(greet, A),
         }
         result = build(artifacts)
-        self.assertEqual(result['AB'], 49)
-        self.assertEqual(result['C minus B'], 3)
-        self.assertEqual(result['msg'], 'Hello World! The answer is 42.')
+        self.assertEqual(result["AB"], 49)
+        self.assertEqual(result["C minus B"], 3)
+        self.assertEqual(result["msg"], "Hello World! The answer is 42.")
 
     def test_partial_build(self):
         artifacts = {
-            'A': lambda x: x**2,
-            'B': lambda A, x: A(x),
-            'C': lambda A, x: 'A(4)-{} is equal to {}'.format(x, A(4)-x)
+            "A": lambda x: x ** 2,
+            "B": lambda A, x: A(x),
+            "C": lambda A, x: "A(4)-{} is equal to {}".format(x, A(4) - x),
         }
         result = build(artifacts, allow_partial_functions=True)
-        self.assertTrue(callable(result['A']))
-        self.assertEqual(result['A'](4), 16)
-        self.assertEqual(result['B'](-5), 25)
-        self.assertEqual(result['C'](6), 'A(4)-6 is equal to 10')
+        self.assertTrue(callable(result["A"]))
+        self.assertEqual(result["A"](4), 16)
+        self.assertEqual(result["B"](-5), 25)
+        self.assertEqual(result["C"](6), "A(4)-6 is equal to 10")
 
     def test_build_with_partial_functions(self):
-        artifacts = {
-            'a': 42,
-            'b': lambda A: A*2
-        }
+        artifacts = {"a": 42, "b": lambda A: A * 2}
         with self.assertRaises(UnresolvedDependencyError):
-            _ = build(artifacts, solver='linear')
+            _ = build(artifacts, solver="linear")
 
         result = build(artifacts, allow_partial_functions=True)
-        self.assertIsInstance(result['b'], partial)
+        self.assertIsInstance(result["b"], partial)
 
     # TODO: fix those!
     # def test_special_keys(self):
@@ -100,48 +96,61 @@ class BuildTest(unittest.TestCase):
         def subtract(p, q):
             return p - q
 
-        result = build({
-            'p': 3,
-            'q': 5,
-            'p - q': subtract,
-            'q - p': lambda p, q: subtract(q, p),
-        })
+        result = build(
+            {
+                "p": 3,
+                "q": 5,
+                "p - q": subtract,
+                "q - p": lambda p, q: subtract(q, p),
+            }
+        )
 
-        self.assertEqual(result['p - q'], -2)
-        self.assertEqual(result['q - p'], 2)
+        self.assertEqual(result["p - q"], -2)
+        self.assertEqual(result["q - p"], 2)
 
-        result = build({
-            'a': -11,
-            'b': 7.5,
-            'a - b': At('a', 'b', subtract),
-            'b - a': At('b', 'a', subtract),
-        })
+        result = build(
+            {
+                "a": -11,
+                "b": 7.5,
+                "a - b": At("a", "b", subtract),
+                "b - a": At("b", "a", subtract),
+            }
+        )
 
-        self.assertEqual(result['a - b'], -18.5)
-        self.assertEqual(result['b - a'], 18.5)
+        self.assertEqual(result["a - b"], -18.5)
+        self.assertEqual(result["b - a"], 18.5)
 
     def test_deep_build(self):
-        for solver in ['linear', 'bfs', 'bfs_parallel', 'async']:
-            results = build({
-                'a': 'a',
-                'b': 'b',
-                'x': lambda a: 'x',
-                'c': lambda x, b: 'c',
-            }, solver=solver)
+        for solver in ["linear", "bfs", "bfs_parallel", "async"]:
+            results = build(
+                {
+                    "a": "a",
+                    "b": "b",
+                    "x": lambda a: "x",
+                    "c": lambda x, b: "c",
+                },
+                solver=solver,
+            )
 
-            self.assertEqual(results['c'], 'c')
+            self.assertEqual(results["c"], "c")
 
     def test_solvers(self):
         def subtract(p, q):
             return p - q
 
-        solvers = ['linear', 'bfs', 'bfs_parallel', 'async']
-        results = [build({
-            'a': -11,
-            'b': 7.5,
-            'a - b': At('a', 'b', subtract),
-            'b - a': At('b', 'a', subtract),
-        }, solver=solver) for solver in solvers]
+        solvers = ["linear", "bfs", "bfs_parallel", "async"]
+        results = [
+            build(
+                {
+                    "a": -11,
+                    "b": 7.5,
+                    "a - b": At("a", "b", subtract),
+                    "b - a": At("b", "a", subtract),
+                },
+                solver=solver,
+            )
+            for solver in solvers
+        ]
 
         # make sure we have as many results as there are solvers
         self.assertEqual(len(results), len(solvers))
@@ -152,5 +161,5 @@ class BuildTest(unittest.TestCase):
         self.assertEqual(len(result_set), 1)
 
         result = json.loads(next(iter(result_set)))
-        self.assertEqual(result['a - b'], -18.5)
-        self.assertEqual(result['b - a'], 18.5)
+        self.assertEqual(result["a - b"], -18.5)
+        self.assertEqual(result["b - a"], 18.5)
--- test/test_models.py
+++ test/test_models.py
@@ -8,73 +8,72 @@ from artifax.utils import At
 
 
 class ModelTest(unittest.TestCase):
-
     def test_add(self):
         afx = Artifax()
-        afx.set('a', 42)
+        afx.set("a", 42)
         self.assertEqual(len(afx), 1)
         self.assertEqual(afx.number_of_nodes(), 1)
         self.assertEqual(afx.number_of_edges(), 0)
-        self.assertTrue('a' in afx)
+        self.assertTrue("a" in afx)
 
-        afx.set('x', lambda a: a/3.14)
+        afx.set("x", lambda a: a / 3.14)
         self.assertEqual(len(afx), 2)
         self.assertEqual(afx.number_of_nodes(), 2)
         self.assertEqual(afx.number_of_edges(), 1)
-        self.assertTrue('x' in afx)
+        self.assertTrue("x" in afx)
 
-        self.assertFalse('y' in afx)
+        self.assertFalse("y" in afx)
 
     def test_pop(self):
         afx = Artifax()
-        afx.set('c', 'C')
-        c = afx.pop('c')
+        afx.set("c", "C")
+        c = afx.pop("c")
         self.assertTrue(len(afx) == 0)
-        self.assertEqual(c, 'C')
+        self.assertEqual(c, "C")
 
     def test_build(self):
         obj = object()
-        afx = Artifax({
-            'int': 42,
-            'float': 1.618,
-            'string': 'Hello',
-            'obj': obj,
-            'list': [1,2,3],
-            'dictionary': {'answer': 42},
-            'set': set([1,2,3.14])
-        })
-        result = afx.build(solver='linear')
-        self.assertEqual(result['int'], 42)
-        self.assertEqual(result['float'], 1.618)
-        self.assertEqual(result['string'], 'Hello')
-        self.assertEqual(result['obj'], obj)
-        self.assertEqual(result['list'], [1,2,3])
-        self.assertEqual(result['dictionary'], {'answer': 42})
-        self.assertEqual(result['set'], set([1,2,3.14]))
+        afx = Artifax(
+            {
+                "int": 42,
+                "float": 1.618,
+                "string": "Hello",
+                "obj": obj,
+                "list": [1, 2, 3],
+                "dictionary": {"answer": 42},
+                "set": set([1, 2, 3.14]),
+            }
+        )
+        result = afx.build(solver="linear")
+        self.assertEqual(result["int"], 42)
+        self.assertEqual(result["float"], 1.618)
+        self.assertEqual(result["string"], "Hello")
+        self.assertEqual(result["obj"], obj)
+        self.assertEqual(result["list"], [1, 2, 3])
+        self.assertEqual(result["dictionary"], {"answer": 42})
+        self.assertEqual(result["set"], set([1, 2, 3.14]))
 
     def test_invalid_build(self):
-        artifacts = {
-            'a': 42,
-            'b': lambda A: A*2
-        }
+        artifacts = {"a": 42, "b": lambda A: A * 2}
         afx = Artifax(artifacts)
         with self.assertRaises(UnresolvedDependencyError):
-            _ = afx.build(solver='bfs_parallel')
+            _ = afx.build(solver="bfs_parallel")
 
         result = afx.build(allow_partial_functions=True)
-        self.assertIsInstance(result['b'], partial)
+        self.assertIsInstance(result["b"], partial)
 
     def test_incremental_build(self):
         class ExpensiveObject:
             def __init__(self):
                 self.counter = 0
+
             def expensive_method(self, _):
                 self.counter += 1
-                return 'foobar'
+                return "foobar"
 
         exo = ExpensiveObject()
         afx = Artifax(
-            p=(3,4),
+            p=(3, 4),
             q=(12, 13),
             exo=lambda q: exo.expensive_method(q),
         )
@@ -82,96 +81,107 @@ class ModelTest(unittest.TestCase):
         # pool_async silently fails to get trigger callback that resolves
         # the nodes, I guess because it can't pickle ExpensiveObject
         # from the unittest thread
-        result = afx.build(solver='linear')
+        result = afx.build(solver="linear")
         self.assertEqual(exo.counter, 1)
 
-        afx.set('p', (1,1))
-        result = afx.build(solver='linear')
+        afx.set("p", (1, 1))
+        result = afx.build(solver="linear")
         self.assertEqual(exo.counter, 1)
 
-        afx.set('q', (0,0))
-        result = afx.build(solver='linear')
+        afx.set("q", (0, 0))
+        result = afx.build(solver="linear")
         self.assertEqual(exo.counter, 2)
 
-        afx.set('new', 'hello')
-        result = afx.build(solver='linear')
-        self.assertEqual(result['new'], 'hello')
+        afx.set("new", "hello")
+        result = afx.build(solver="linear")
+        self.assertEqual(result["new"], "hello")
         self.assertEqual(exo.counter, 2)
 
-        afx.pop('new')
-        result = afx.build(solver='linear')
+        afx.pop("new")
+        result = afx.build(solver="linear")
         self.assertEqual(exo.counter, 2)
 
     def test_initial(self):
-        afx = Artifax({
-            'earth': object(),
-            'un': lambda earth: 'water @{}'.format(earth),
-            'mars': object(),
-            'mcrn': lambda mars: 'dust @{}'.format(mars),
-            'belt': object(),
-            'opa': lambda belt: 'ice @{}'.format(belt)
-        })
-        self.assertEqual(afx.initial(), set(['earth', 'mars', 'belt']))
+        afx = Artifax(
+            {
+                "earth": object(),
+                "un": lambda earth: "water @{}".format(earth),
+                "mars": object(),
+                "mcrn": lambda mars: "dust @{}".format(mars),
+                "belt": object(),
+                "opa": lambda belt: "ice @{}".format(belt),
+            }
+        )
+        self.assertEqual(afx.initial(), set(["earth", "mars", "belt"]))
         _ = afx.build()
 
     def test_multiprocessing(self):
-        afx = Artifax({
-            'p1': 'p1',
-            'p2': 'p2',
-            'c1': lambda p1: 'c1 after {}'.format(p1),
-            'c2': lambda p1: None,
-            'c3': lambda p2: None,
-            'c4': lambda p2: None,
-            'c5': lambda p1, p2, c2, c3: None,
-            'c6': lambda c1: None,
-            'F':  lambda c1, c2, c3, c4, c5: None,
-        })
+        afx = Artifax(
+            {
+                "p1": "p1",
+                "p2": "p2",
+                "c1": lambda p1: "c1 after {}".format(p1),
+                "c2": lambda p1: None,
+                "c3": lambda p2: None,
+                "c4": lambda p2: None,
+                "c5": lambda p1, p2, c2, c3: None,
+                "c6": lambda c1: None,
+                "F": lambda c1, c2, c3, c4, c5: None,
+            }
+        )
 
-        c1 = afx.build(targets=('c1',))
-        self.assertEqual(c1, 'c1 after p1')
+        c1 = afx.build(targets=("c1",))
+        self.assertEqual(c1, "c1 after p1")
 
     def test_in_operator(self):
-        afx = Artifax(p=(3,4))
-        self.assertTrue('p' in afx)
-        self.assertFalse('q' in afx)
+        afx = Artifax(p=(3, 4))
+        self.assertTrue("p" in afx)
+        self.assertFalse("q" in afx)
 
-        afx.pop('p')
-        self.assertFalse('p' in afx)
+        afx.pop("p")
+        self.assertFalse("p" in afx)
 
-        afx.set('q', (1,2))
-        self.assertTrue('q' in afx)
+        afx.set("q", (1, 2))
+        self.assertTrue("q" in afx)
 
     def test_targeted_build(self):
         afx = Artifax(
-            {'greeting': lambda name, punctuation: 'Hello, {}{}'.format(name, punctuation)},
-            name='World',
-            punctuation=''
+            {
+                "greeting": lambda name, punctuation: "Hello, {}{}".format(
+                    name, punctuation
+                )
+            },
+            name="World",
+            punctuation="",
         )
-        greeting = afx.build(targets='greeting')
-        self.assertEqual(greeting, 'Hello, World')
+        greeting = afx.build(targets="greeting")
+        self.assertEqual(greeting, "Hello, World")
 
-        afx.set('punctuation', '!')
-        greeting, punctuation = afx.build(targets=('greeting', 'punctuation'))
-        self.assertEqual(greeting, 'Hello, World!')
-        self.assertEqual(punctuation, '!')
+        afx.set("punctuation", "!")
+        greeting, punctuation = afx.build(targets=("greeting", "punctuation"))
+        self.assertEqual(greeting, "Hello, World!")
+        self.assertEqual(punctuation, "!")
 
         result = afx.build()
-        self.assertEqual(result['greeting'], 'Hello, World!')
-        self.assertEqual(result['name'], 'World')
-        self.assertEqual(result['punctuation'], '!')
+        self.assertEqual(result["greeting"], "Hello, World!")
+        self.assertEqual(result["name"], "World")
+        self.assertEqual(result["punctuation"], "!")
 
     def test_stale(self):
         class C:
             counter = 0
+
             def __init__(self):
                 C.counter += 1
 
-        afx = Artifax({
-            'a': 42,
-            'b': lambda a: math.pow(a, 5),
-            'counter': lambda: C(),
-        })
-        result = afx.build(targets='b')
+        afx = Artifax(
+            {
+                "a": 42,
+                "b": lambda a: math.pow(a, 5),
+                "counter": lambda: C(),
+            }
+        )
+        result = afx.build(targets="b")
         self.assertEqual(result, 130691232)
 
         # 'counter' node should not have been evaluated
@@ -180,7 +190,7 @@ class ModelTest(unittest.TestCase):
 
         # cannot use async solver here because C won't be
         # pickable from pyunit
-        _ = afx.build()['counter']()
+        _ = afx.build()["counter"]()
         self.assertEqual(C.counter, 1)
 
     def test_at_constructor(self):
@@ -188,12 +198,12 @@ class ModelTest(unittest.TestCase):
             return p - q
 
         afx = Artifax(
-            ab=At('a', 'b', subtract),
-            ba=At('b', 'a', subtract),
+            ab=At("a", "b", subtract),
+            ba=At("b", "a", subtract),
             a=-11,
             b=7.5,
         )
 
-        ab, ba = afx.build(targets=('ab', 'ba'))
+        ab, ba = afx.build(targets=("ab", "ba"))
         self.assertEqual(ab, -18.5)
         self.assertEqual(ba, 18.5)
