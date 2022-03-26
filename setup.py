f056467c81fc1414176eb1167f6873654220782e
Bruno Lange <blangeram@gmail.com>
2022-03-25 22:53:45 -0700

    Registers version 0.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="artifax",
-    version="0.3",
+    version="0.4",
     packages=["artifax"],
     description="python package for building artifacts from a computational graph",
     long_description=long_description,
